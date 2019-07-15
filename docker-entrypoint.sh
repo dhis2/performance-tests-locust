699fa9e0634d00cbcf8cfa5747afd4f0df8ef8d2
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-07-15 14:03:07 +0200

    Updating docker-entrypoint and optimising dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 set -e
-ADDITIONAL_OPTS="$OPTS:-"
-LOCUST_OPTS="-f $LOCUST_FILE --master --host=$HOST"
+
+ADDITIONAL_OPTS=""
+
+if [ ! -z "$TIME" ]; then
+    ADDITIONAL_OPTS="$ADDITIONAL_OPTS -t $TIME"
+fi
+
+LOCUST_OPTS="-f $LOCUST_FILE --master --host=$HOST --no-web --csv=dhis -c $USERS -r $HATCH_RATE $ADDITIONAL_OPTS"
 
 cd /locust
 locust ${LOCUST_OPTS}
