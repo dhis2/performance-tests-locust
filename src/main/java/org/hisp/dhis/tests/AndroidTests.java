50cb34371441b07630bc20b8e2f81c333a5bd2c0
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-10-28 16:17:59 +0200

    test: adds android test for downloading latest events (#48)

--- src/main/java/org/hisp/dhis/tests/AndroidTests.java
+++ src/main/java/org/hisp/dhis/tests/AndroidTests.java
@@ -2,6 +2,7 @@ package org.hisp.dhis.tests;
 
 import org.hisp.dhis.tasks.DhisAbstractTask;
 import org.hisp.dhis.tasksets.aggregate.Android_syncDataValuesTaskSet;
+import org.hisp.dhis.tasksets.tracker.Android_downloadLatestEventsTaskSet;
 import org.hisp.dhis.tasksets.tracker.Android_downloadLatestTeisTaskSet;
 import org.hisp.dhis.tasksets.tracker.Android_syncTeisTaskSet;
 
@@ -21,7 +22,8 @@ public class AndroidTests
         return Arrays.asList(
             new Android_syncTeisTaskSet( 1, 10 ),
             new Android_syncDataValuesTaskSet( 1 ),
-            new Android_downloadLatestTeisTaskSet( 1 )
+            new Android_downloadLatestTeisTaskSet( 1 ),
+            new Android_downloadLatestEventsTaskSet( 1 )
         );
     }
 }
