6fded084931b6b3b21d77305872963ad199f390d
radnov <radnov@pm.me>
2021-11-02 14:48:20 +0200

    build: Remove custom reporting and use default headless reports [DEVOPS-30]

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,49 +1,38 @@
 @Library('pipeline-library') _
+
 pipeline {
     agent {
         label 'ec2-jdk11'
     }
 
     environment {
-//         GIT_URL = "https://github.com/dhis2/performance-tests-locust"
 //         AWX_BOT_CREDENTIALS = credentials('awx-bot-user-credentials')
-//         GITHUB_CREDS = credentials('github_bot')
-//         GITHUB_USERNAME = "${GITHUB_CREDS_USR}"
-//         GITHUB_TOKEN = credentials('github-token')
-//         IMAGE_NAME = "dhis2/locustio"
-//         IMAGE_TAG = "latest"
-        COMPOSE_ARGS = "NO_WEB=true TIME=30m HATCH_RATE=5 USERS=30"
-        LOCUST_REPORT_DIR = "locust"
-//         LOCAL_REPORT_DIR = "reports"
-        REPORT_FILE = "html_report.html"
+        IMAGE_NAME = "dhis2/locustio-test"
+        IMAGE_TAG = "latest"
+        COMPOSE_ARGS = "NO_WEB=true TIME=30s HATCH_RATE=1 USERS=5"
+        LOCUST_REPORT_DIR = "reports"
+        HTML_REPORT_FILE = "test_report.html"
         INSTANCE_HOST = "test.performance.dhis2.org"
         INSTANCE_NAME = "2.37.0"
     }
 
-//     triggers {
-//        cron('H 3 * * *')
-//     }
-
     stages {
-//         stage('Checkout') {
-//             steps {
-//                 git branch: 'DEVOPS-30', url: "${GIT_URL}"
-//             }
-//         }
-
-//         stage('Update performance test instance') {
-//             steps {
+        stage('Update performance test instance') {
+            steps {
+                echo 'Updating performance test instance ...'
 //                 script {
 //                     awx.resetWar("$AWX_BOT_CREDENTIALS", "${INSTANCE_HOST}", "${INSTANCE_NAME}")
 //                 }
-//             }
-//         }
+            }
+        }
 
         stage('Start locust') {
             steps {
                 script {
-//                     sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+                    sh "mkdir -p ${LOCUST_REPORT_DIR}"
+                    sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ./docker"
                     sh "${COMPOSE_ARGS} docker-compose up -d"
+                    sh "ls -la"
                 }
             }
         }
@@ -51,7 +40,7 @@ pipeline {
         stage('Run tests') {
             steps {
                 script {
-                    sh "mvn -s settings.xml clean compile exec:java -Dtarget.baseuri=https://$INSTANCE_HOST/$INSTANCE_NAME"
+                    sh "mvn clean compile exec:java -Dtarget.base_uri=https://$INSTANCE_HOST/$INSTANCE_NAME"
                 }
             }
         }
@@ -59,19 +48,16 @@ pipeline {
 
     post {
         always {
-            script {
-//                 sh "mkdir -p ${LOCAL_REPORT_DIR} && cp ./${LOCUST_REPORT_DIR}/${REPORT_FILE} ./${LOCAL_REPORT_DIR}/${REPORT_FILE}"
-                publishHTML target: [
-                        allowMissing: false,
-                        alwaysLinkToLastBuild: false,
-                        keepAll: true,
-                        reportDir: "${LOCUST_REPORT_DIR}",
-                        reportFiles: "${REPORT_FILE}",
-                        reportName: 'Load test report'
-                ]
+            archiveArtifacts artifacts: "${LOCUST_REPORT_DIR}/*.csv"
 
-//                 sh "docker-compose down -v"
-            }
+            publishHTML target: [
+                allowMissing: false,
+                alwaysLinkToLastBuild: false,
+                keepAll: true,
+                reportDir: "${LOCUST_REPORT_DIR}",
+                reportFiles: "${HTML_REPORT_FILE}",
+                reportName: 'Load test report'
+            ]
         }
     }
 }
--- docker/locust-master.py
+++ docker/locust-master.py
@@ -24,85 +24,85 @@ class MyTaskSet(locust.TaskSet):
 class MyUser(locust.User):
     tasks = [MyTaskSet]
 
-@events.quitting.add_listener
-def handle_quit(**kw):
-    generate_report()
-
-def generate_report():
-    print('Generating html report')
-    loader = FileSystemLoader('/templates')
-    env = Environment(loader=loader)
-
-    csv.register_dialect('dialect',
-                         delimiter=',',
-                         quotechar='"',
-                         quoting=csv.QUOTE_ALL,
-                         skipinitialspace=True)
-
-    # write failures to file
-    with open(WORK_DIR + CSV_PREFIX + '_errors.csv', "w") as f:
-        f.write(failures_csv())
-
-    r_data, e_data, dist_data = {}, {}, {}
-    r_headers, e_headers, dist_headers = [], [], []
-
-    def store_data(file, headers, data):
-        with open(WORK_DIR + CSV_PREFIX + file) as f:
-            content = csv.DictReader(f, dialect='dialect')
-            headers.extend(content.fieldnames)
-            for row in content:
-                for field_name in headers:
-                    if field_name not in data:
-                        data[field_name] = []
-                    data[field_name].append(row[field_name])
-
-    store_data('_requests.csv', r_headers, r_data)
-    store_data('_errors.csv', e_headers, e_data)
-    store_data('_distribution.csv', dist_headers, dist_data)
-
-    # compute failure rate based on total stats
-    if 'Total' in r_data['Name']:
-        index = r_data['Name'].index('Total')
-        total_rps = r_data['Requests/s'][index]
-        total_failures = r_data['# failures'][index]
-        total_requests = r_data['# requests'][index]
-        fail_percent = (float(total_failures) / float(total_requests + total_failures)) * 100
-
-    # write report
-    with open(WORK_DIR + HTML_REPORT_NAME, "w") as report:
-        report.write(env.get_template(TEMPLATE_NAME).render(
-            datetime=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
-            rps=total_rps,
-            fails=str(fail_percent) + "%",
-            stat_headers=r_headers,
-            stat_data=r_data.values(),
-            e_headers=e_headers,
-            e_data=e_data.values(),
-            dist_headers=dist_headers,
-            dist_data=dist_data.values()
-        ))
-
-
-def failures_csv():
-    rows = [
-        ",".join((
-            '"Method"',
-            '"Name"',
-            '"Error"',
-            '"Occurrences"',
-        ))
-    ]
-
-    errors = runners.locust_runner.stats.serialize_errors()
-
-    for s in errors:
-        er = errors[s];
-        print(er)
-        rows.append('"%s","%s","%s",%i' % (
-            er['method'],
-            er['name'],
-            er['error'].replace("\"", "'"),
-            er['occurences'],
-        ))
-    return "\n".join(rows)
+# @events.quitting.add_listener
+# def handle_quit(**kw):
+#     generate_report()
+#
+# def generate_report():
+#     print('Generating html report')
+#     loader = FileSystemLoader('/templates')
+#     env = Environment(loader=loader)
+#
+#     csv.register_dialect('dialect',
+#                          delimiter=',',
+#                          quotechar='"',
+#                          quoting=csv.QUOTE_ALL,
+#                          skipinitialspace=True)
+#
+#     # write failures to file
+#     with open(WORK_DIR + CSV_PREFIX + '_errors.csv', "w") as f:
+#         f.write(failures_csv())
+#
+#     r_data, e_data, dist_data = {}, {}, {}
+#     r_headers, e_headers, dist_headers = [], [], []
+#
+#     def store_data(file, headers, data):
+#         with open(WORK_DIR + CSV_PREFIX + file) as f:
+#             content = csv.DictReader(f, dialect='dialect')
+#             headers.extend(content.fieldnames)
+#             for row in content:
+#                 for field_name in headers:
+#                     if field_name not in data:
+#                         data[field_name] = []
+#                     data[field_name].append(row[field_name])
+#
+#     store_data('_requests.csv', r_headers, r_data)
+#     store_data('_errors.csv', e_headers, e_data)
+#     store_data('_distribution.csv', dist_headers, dist_data)
+#
+#     # compute failure rate based on total stats
+#     if 'Total' in r_data['Name']:
+#         index = r_data['Name'].index('Total')
+#         total_rps = r_data['Requests/s'][index]
+#         total_failures = r_data['# failures'][index]
+#         total_requests = r_data['# requests'][index]
+#         fail_percent = (float(total_failures) / float(total_requests + total_failures)) * 100
+#
+#     # write report
+#     with open(WORK_DIR + HTML_REPORT_NAME, "w") as report:
+#         report.write(env.get_template(TEMPLATE_NAME).render(
+#             datetime=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+#             rps=total_rps,
+#             fails=str(fail_percent) + "%",
+#             stat_headers=r_headers,
+#             stat_data=r_data.values(),
+#             e_headers=e_headers,
+#             e_data=e_data.values(),
+#             dist_headers=dist_headers,
+#             dist_data=dist_data.values()
+#         ))
+#
+#
+# def failures_csv():
+#     rows = [
+#         ",".join((
+#             '"Method"',
+#             '"Name"',
+#             '"Error"',
+#             '"Occurrences"',
+#         ))
+#     ]
+#
+#     errors = runners.locust_runner.stats.serialize_errors()
+#
+#     for s in errors:
+#         er = errors[s];
+#         print(er)
+#         rows.append('"%s","%s","%s",%i' % (
+#             er['method'],
+#             er['name'],
+#             er['error'].replace("\"", "'"),
+#             er['occurences'],
+#         ))
+#     return "\n".join(rows)
 
