1c1efe980e6ebe2dacd297afbcc59564b60bf9e8
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2022-08-16 21:08:44 +0200

    fix: idk why it's there

--- src/main/java/org/hisp/dhis/random/TrackedEntityAttributeRandomizer.java
+++ src/main/java/org/hisp/dhis/random/TrackedEntityAttributeRandomizer.java
@@ -121,7 +121,6 @@ public class TrackedEntityAttributeRandomizer
             value = getValueSegment( textPattern ).getParameter() + value;
         }
 
-        value = "";
         return value;
     }
 
